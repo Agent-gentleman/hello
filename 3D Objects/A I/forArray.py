641bf2066ed282779d9be337e7dc7d3fbf233ca2
M.Hamza Gohar <iamhamzagohar6914@gmail.com>
2024-04-20 16:20:54 +0500

    added a python file

---
+++ 3D Objects/A I/forArray.py
@@ -0,0 +1,21 @@
+
+#accessing elements in an array using for loop
+var=["Toyota","Honda","Suzuki","kia"]
+
+for x in var:
+  print (x)
+
+# printing digits in Toyota
+for x in "Toyota":
+  print (x)
+# to stop or continue an array
+
+for x in var:
+  print(x)
+  if x=="Honda":
+    break
+
+
+
+
+  
